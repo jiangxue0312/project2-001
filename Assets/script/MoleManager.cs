02d1893f87c73a1c604da404799701b9ba8c16fe
sunrock <sunrocksong@gmail.com>
2016-10-17 16:02:54 +1100

    Add dust in desert and normal map

--- Assets/script/MoleManager.cs
+++ Assets/script/MoleManager.cs
@@ -14,14 +14,15 @@ public class MoleManager : MonoBehaviour {
     // Time before hiding the mole
     public float HideDelay;
 
+    private int totalNum = 5;
+
     // Maximum number of moles show in one term
     //public int MaxMole;
 
 	void Start () {
         Debug.Log("ManagerStart");
         InvokeRepeating("MoleShow", StartDelay, ShowDelay);
-	
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -30,16 +31,21 @@ public class MoleManager : MonoBehaviour {
 
     void MoleShow()
     {
-        
-        int random = Random.Range(0, moleList.Count);
-        int random2 = Random.Range(0, moleList.Count);
-        while (random == random2)
-        {
-            random2 = Random.Range(0, moleList.Count);
-        }
+        int random = Random.Range(0, totalNum);
         Debug.Log("random: " + random);
-        Debug.Log("random2: " + random2);
-        moleList[random].GetComponentInChildren<Mole>().Show();
-        moleList[random2].GetComponentInChildren<Mole>().Show();
+        for (int i = 0; i < random; i++)
+        {
+            moleList[Random.Range(0, moleList.Count)].GetComponentInChildren<Mole>().Show();
+        }
+
+        //int random2 = Random.Range(0, moleList.Count);
+        //while (random == random2)
+        //{
+        //    random2 = Random.Range(0, moleList.Count);
+        //}
+        //Debug.Log("random: " + random);
+        //Debug.Log("random2: " + random2);
+        //[random].GetComponentInChildren<Mole>().Show();
+        //moleList[random2].GetComponentInChildren<Mole>().Show();
     }
 }
